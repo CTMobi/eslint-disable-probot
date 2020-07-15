4f170c5c9067578640b3021c80cf1d64bcdf6502
Giuseppe Iuculano <giuseppe.iuculano@ctmobi.it>
2020-07-15 16:45:00 +0200

    Warn also for PHP files

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ module.exports = (robot) => {
 
       for (const file of files.data) {
         let currentPosition = 0
-        if (!file.filename.endsWith('.js')) return
+        if (!file.filename.endsWith('.js') && !file.filename.endsWith('.php')) return
 
         // In order to not spam the PR with comments we'll stop after a certain number of comments
         if (comments.length > commentLimit) return
